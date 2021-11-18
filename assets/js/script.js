b8347ea01c53fe8f1d216b70cd21c445ab8dd023
Mickael Noel <mickael.noel@novei.fr>
2021-11-18 10:45:15 +0100

    Début de l'affichage des infos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 fetch("assets/json/movies.json")
-    .then(toto => toto.json())
-    .then(tata => console.log(tata.results))
+    .then(fetchResponse => fetchResponse.json())
+    .then(jsonContent => {
+        jsonContent.results.map((movie, index) => {
+            //Création de la div contenant les infos du film
+            let movieDiv = document.createElement("div")
+            movieDiv.id = "TMMDB" + movie.id
+            movieDiv.className = "col-3 row"
+            //Ajout de l'image
+            let movieImg = document.createElement("img")
+            movieImg.className = "col-6"
+            movieImg.src = movie.poster_path
+            //Création de la div qui contient les infos du film sauf l'image
+            
+            //Création du h2 contenant le titre du film
+            let movieTitle = document.createElement("h2")
+            movieTitle.innerText = movie.original_title
+            movieTitle.className = "h5"
+            //Accorche des éléments HTML virtuel au DOM
+            movieDiv.appendChild(movieImg)
+            movieDiv.appendChild(movieTitle)
+            movieContent.appendChild(movieDiv)
+        })
+    })
     .catch(err => console.error(err))
